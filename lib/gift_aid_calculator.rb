f034aa9d7d226ccdf96f8fc6e546218af0bfebb0
Jeremy Fox <foxjerem@gmail.com>
2014-09-21 15:08:00 +0100

    refactored 1

--- lib/gift_aid_calculator.rb
+++ lib/gift_aid_calculator.rb
@@ -11,17 +11,21 @@ class GiftAidCalculator
     @@tax_rate
   end
 
-  def self.gift_aid_for(donation_amount, event = default_event)
-    rounded_gift_aid_calculation(donation_amount, event)
-  end
-
   def self.update_tax_rate(new_tax_rate, user)
     set_tax_rate_to(new_tax_rate) if user.administrator?
   end
 
+  def self.gift_aid_for(donation_amount, event = default_event)
+    rounded_gift_aid_calculation(donation_amount, event)
+  end
+
   def self.rounded_gift_aid_calculation(donation_amount, event)
-    return simple_gift_aid_calculation(donation_amount).round(DEFAULT_ROUNDING) unless event.promoted?
-    supplemented_gift_aid_calculation(donation_amount, event).round(DEFAULT_ROUNDING)
+    raw_gift_aid_calculation(donation_amount, event).round(DEFAULT_ROUNDING)
+  end
+
+  def self.raw_gift_aid_calculation(donation_amount, event)
+    return simple_gift_aid_calculation(donation_amount) unless event.promoted?
+    supplemented_gift_aid_calculation(donation_amount, event)
   end
 
   def self.simple_gift_aid_calculation(donation_amount)
@@ -29,11 +33,15 @@ class GiftAidCalculator
   end
 
   def self.supplemented_gift_aid_calculation(donation_amount, event)
-    simple_gift_aid_calculation(donation_amount) * ( 1 + event.gift_aid_supplement / 100 )
+    simple_gift_aid_calculation(donation_amount) * supplement_multiplier_for(event)
   end
 
   def self.gift_aid_multiplier
     tax_rate / (100 - tax_rate)
+  end
+
+  def self.supplement_multiplier_for(event)
+    ( 1 + event.gift_aid_supplement / 100 )
   end
 
   def self.set_tax_rate_to(new_tax_rate)
@@ -45,6 +53,7 @@ class GiftAidCalculator
   end 
 
   private_class_method  :supplemented_gift_aid_calculation,
+                        :raw_gift_aid_calculation,
                         :rounded_gift_aid_calculation,
                         :simple_gift_aid_calculation,
                         :gift_aid_multiplier,
